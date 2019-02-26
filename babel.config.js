38cd934adcb9ac963916b1a8c9e718eca55f6b5e
wen Tim <wentim@wende-MacBook-Pro.local>
2019-02-26 21:59:42 +0800

    fix:移除production console.log

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
+const plugin = []
+if(process.env.NODE_ENV === 'production') {
+  plugin.push("transform-remove-console")
+}
+
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  plugins: plugin
 }
